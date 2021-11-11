df8af893f8284a8dac1bc65acf3510e2c63cd905
lkwate <kwatedassiloic@gmail.com>
2021-11-11 14:16:53 +0100

    add question generation to RL language modelling

--- core/dataset.py
+++ core/dataset.py
@@ -5,6 +5,30 @@ from torch.utils.data import DataLoader
 import datasets
 from loguru import logger
 from typing import List, Union, Dict
+from functools import partial
+
+
+def _collate_fn(features, data_collator):
+    if isinstance(features, list):
+        features = [
+            {key: value.tolist() for key, value in feature.items()}
+            for feature in features
+        ]
+    elif isinstance(features, dict):
+        features = {key: value.tolist() for key, value in features.items()}
+    else:
+        raise ValueError("features should be of type either list of dictionary")
+
+    features = data_collator(features)
+
+    if "labels" in features:
+        features["labels"] = torch.where(
+            features["labels"] == 1,
+            data_collator.label_pad_token_id,
+            features["labels"],
+        )
+
+    return features
 
 
 class MultiNewsLightningDataModule(pl.LightningDataModule):
@@ -23,6 +47,7 @@ class MultiNewsLightningDataModule(pl.LightningDataModule):
         self.data_collator = DataCollatorForSeq2Seq(
             self.tokenizer, max_length=self.max_length, padding="max_length"
         )
+        self.collate_fn = partial(_collate_fn, data_collator=self.data_collator)
 
     def prepare_data(self):
         logger.info("Multi_news dataset loading....")
@@ -69,28 +94,6 @@ class MultiNewsLightningDataModule(pl.LightningDataModule):
 
         return output
 
-    def _collate_fn(self, features):
-        if isinstance(features, list):
-            features = [
-                {key: value.tolist() for key, value in feature.items()}
-                for feature in features
-            ]
-        elif isinstance(features, dict):
-            features = {key: value.tolist() for key, value in features.items()}
-        else:
-            raise ValueError("features should be of type either list of dictionary")
-
-        features = self.data_collator(features)
-
-        if "labels" in features:
-            features["labels"] = torch.where(
-                features["labels"] == 1,
-                self.data_collator.label_pad_token_id,
-                features["labels"],
-            )
-
-        return features
-
     def train_dataloader(
         self,
     ) -> Union[DataLoader, List[DataLoader], Dict[str, DataLoader]]:
@@ -99,14 +102,98 @@ class MultiNewsLightningDataModule(pl.LightningDataModule):
             batch_size=self.batch_size,
             num_workers=self.num_workers,
             shuffle=True,
+            collate_fn=self.collate_fn,
         )
 
     def val_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
         return DataLoader(
-            self.validation, batch_size=self.batch_size, num_workers=self.num_workers
+            self.validation,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            collate_fn=self.collate_fn,
         )
 
     def test_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
         return DataLoader(
-            self.test, batch_size=self.batch_size, num_workers=self.num_workers
+            self.test,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            collate_fn=self.collate_fn,
+        )
+
+
+class SQuADLightningDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        tokenizer: AutoTokenizer,
+        batch_size: int,
+        num_workers: int,
+        max_length: int,
+    ):
+        super().__init__()
+        self.tokenizer = tokenizer
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.max_length = max_length
+        self.data_collator = DataCollatorForSeq2Seq(
+            self.tokenizer, max_length=self.max_length, padding="max_length"
+        )
+        self.collate_fn = partial(_collate_fn, data_collator=self.data_collator)
+
+    def _transform(self, item):
+        context, question, answer = (
+            item["context"],
+            item["question"],
+            map(lambda x: x["text"][0], item["answers"]),
+        )
+        input_text = [a + self.tokenizer.cls_token + c for a, c in zip(context, answer)]
+        context = self.tokenizer(input_text, truncation=True, padding="max_length")
+        question = self.tokenizer(question, truncation=True, padding="max_length")
+
+        output = {
+            "input_ids": context["input_ids"],
+            "attention_mask": context["attention_mask"],
+            "labels": question["input_ids"],
+            "decoder_attention_mask": question["attention_mask"],
+        }
+        return output
+
+    def prepare_data(self) -> None:
+
+        logger.info("Loading SQuAD dataset...")
+        self.dataset = datasets.load_dataset("squad")
+
+        self.train, self.validation = self.dataset["train"], self.dataset["validation"]
+        self.columns = [
+            "input_ids",
+            "attention_mask",
+            "decoder_attention_mask",
+            "labels",
+        ]
+
+        logger.info("Data transformation...")
+        self.train = self.train.map(self._transform, batched=True)
+        self.train.set_format(type="torch", columns=self.columns)
+
+        self.validation = self.validation.map(self._transform, batched=True)
+        self.validation.set_format(type="torch", columns=self.columns)
+        logger.info("Data transformation completed.")
+
+    def train_dataloader(
+        self,
+    ) -> Union[DataLoader, List[DataLoader], Dict[str, DataLoader]]:
+        return DataLoader(
+            self.train,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.num_workers,
+            collate_fn=self.collate_fn,
+        )
+
+    def val_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
+        return DataLoader(
+            self.validation,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            collate_fn=self.collate_fn,
         )
---
+++ train-launcher-question-generation.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+python3 core/question_generation.py roberta-base core/local_actions/local_action_index.csv log_files --batch_size=4
